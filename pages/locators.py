002671327b2df4e5673d1717a2994656cad7b1ad
alopatuk <a.lopatyuk@it.ecommpay.com>
2020-07-01 20:27:41 +0300

    add locators for login page and login page

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,16 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "#login_form>button")
+
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_REPLAY_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "#register_form>button")
---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+
+    def should_be_login_url(self):
+        get_url = self.browser.current_url
+        assert get_url == "login", "Url is not correct"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL), "Login is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Password is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Button for login is not present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Email for registration is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD), "Password for registration is not " \
+                                                                                  "presented "
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_REPLAY_PASSWORD), "Replay password for " \
+                                                                                         "registration is not " \
+                                                                                         "presented "
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON), "Button for registration is not " \
+                                                                                "presented "
